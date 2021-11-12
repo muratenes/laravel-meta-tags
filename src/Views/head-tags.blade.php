b2c497b429ab2b4cc16349f998ce8d66af4f6b68
muratenes <m9urat@gmail.com>
2021-11-12 17:25:39 +0300

    property key changed

--- src/Views/head-tags.blade.php
+++ src/Views/head-tags.blade.php
@@ -2,8 +2,8 @@
     <title>{{ $item->meta_tag->meta_title }}</title>
 @endif
 @foreach(\MuratEnes\LaravelMetaTags\Traits\MetaTaggable::getFieldsByType() as $name => $field)
-    <meta name="{{ $name }}" content="{{ $item->meta_tag->{$name} }}"/>
+    <meta name="{{ $field['key'] }}" content="{{ $item->meta_tag->{$name} }}"/>
 @endforeach
 @foreach(\MuratEnes\LaravelMetaTags\Traits\MetaTaggable::getFieldsByType('meta-property') as $name => $field)
-    <meta property="{{ $name }}" content="{{ $item->meta_tag->{$name} }}"/>
+    <meta property="{{ $field['key'] }}" content="{{ $item->meta_tag->{$name} }}"/>
 @endforeach
